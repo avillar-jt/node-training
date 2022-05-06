93d3da5e9ed3530b2028dac00b451de7bb4f5868
Adrian Villar <adrian.villar@jobandtalent.com>
2022-05-06 14:01:09 +0200

    init express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotEnv from 'dotenv';
+
+dotEnv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server started at http://localhost:${process.env.PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
