5a1f6525948134f507a13b116e41693e578dc1d7
Adrian Villar <adrian.villar@jobandtalent.com>
2022-05-09 11:06:29 +0200

    modules alias settings

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createServer } from './infra/primary-inputs/express.server'
-
-//context
+import 'module-alias/register';
+import { createServer } from '@infra/primary-inputs/express.server'
 import dotEnv from 'dotenv';
+
+//env context
+
 dotEnv.config();
 
 //run express (primary-input)
