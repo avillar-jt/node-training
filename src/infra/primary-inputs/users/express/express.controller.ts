24a31aab10d93e97a5cf5f58495c525f0e85ac07
Adrian Villar <adrian.villar@jobandtalent.com>
2022-05-12 09:48:17 +0200

    !params

--- src/infra/primary-inputs/users/express/express.controller.ts
+++ src/infra/primary-inputs/users/express/express.controller.ts
@@ -4,7 +4,6 @@ import { findUsersUsecase } from '@application/users/usecases/findUsers.usecase'
 import UserDb from '@infra/secondary-outputs/type-orm/repositories/user.datasource';
 
 const findAllUsers = (async (req: any, res: any) => {
-  const params = req.params
   const userRepository = new UserDb()
   const users : User[] = await findUsersUsecase(userRepository)
   res.json(users)
@@ -12,7 +11,6 @@ const findAllUsers = (async (req: any, res: any) => {
 
 const createUser = (async (req: any, res: any) => {
   const userRepository = new UserDb()
-  const params = req.params
   const body = req.body
   //avoid validation
   const user : User = body
